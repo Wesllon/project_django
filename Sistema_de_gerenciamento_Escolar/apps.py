191154acd9d18c8bddc6185c634441653151823b
fabiojesus <fabiojs_2005@yahoo.com.br>
2024-06-13 14:03:55 -0300

    Adicionando as Pasta

---
+++ Sistema_de_gerenciamento_Escolar/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SistemaDeGerenciamentoEscolarConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Sistema_de_gerenciamento_Escolar'
