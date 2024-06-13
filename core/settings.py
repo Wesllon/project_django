7e64c9c5deab177d8661094c05cfe97df3e1bf29
fabiojesus <fabiojs_2005@yahoo.com.br>
2024-06-13 15:14:07 -0300

    Configurações do Django

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Sistema_de_gerenciamento_Escolar'
 ]
 
 MIDDLEWARE = [
@@ -116,6 +117,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS =[
+    BASE_DIR / 'base_static'
+]
+
+STATIC_ROOT = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('Sistema_de_gerenciamento_Escolar.urls'))
+    # path('teste/', my_view)
 ]
