e151839e215ffb9351d2794ef4e121a206d48bc9
fabiojesus <fabiojs_2005@yahoo.com.br>
2024-06-20 15:14:44 -0300

    Atualizando o codigo

--- Sistema_de_gerenciamento_Escolar/admin.py
+++ Sistema_de_gerenciamento_Escolar/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Categoria_produto,Produtos
 # Register your models here.
+
+class Admin_Categoria_produto(admin.ModelAdmin):
+    pass
+@admin.register(Produtos)
+class Admin_Produtos(admin.ModelAdmin): #Opção dois
+    ...
+
+admin.site.register(Categoria_produto,Admin_Categoria_produto)
--- Sistema_de_gerenciamento_Escolar/models.py
+++ Sistema_de_gerenciamento_Escolar/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Categoria_produto(models.Model):
+        nome_categoria = models.CharField(max_length=50)
+        def __str__(self) -> str:
+          return  self.nome_categoria
+
+class Produtos(models.Model):
+    nome_produto = models.CharField(max_length=40)
+    quantidade_produto = models.IntegerField()
+    valor_produto = models.FloatField()
+    data_vencimento = models.DateField(auto_now_add=True)
+    img_produto = models.ImageField(upload_to='Sistema_de_gerenciamento_Escolar/images/%Y/%m/%d/')
+    categoria = models.ForeignKey(Categoria_produto,on_delete= models.SET_NULL,null=True)
+    dono = models.ForeignKey(User,on_delete= models.SET_NULL,null=True)
